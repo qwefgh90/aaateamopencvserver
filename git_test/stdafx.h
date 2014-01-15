8aa216fce25ee61a0c18d99fe7177f26259d551d
Changwon Choe <qwefgh90@google.com>
2014-01-15 19:59:48 +0900

    1)Changwon protocol values add

--- git_test/stdafx.h
+++ git_test/stdafx.h
@@ -28,5 +28,8 @@ typedef struct _Memory{
 	u_char* buf;
 	u_int len;
 }Memory;
-
+typedef enum{LOGIN=1,SIGNUP=2,LOGOUT=3,LEAVE=4,SEARCH=5,MORE=6,
+	WRITE_COMMENT=7,MODIFY_COMMENT=8,DELETE_COMMENT=9,LIKE=10,DISLIKE=11,REPORT=12}PROTO_TYPE;
+u_char spliter[2]={'\r','\n'};
+u_char spliter_end[4]={'\r','\n','\r','\n'};
 #endif

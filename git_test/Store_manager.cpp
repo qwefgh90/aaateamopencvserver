8cf12f0f0b17f6233acfedded4381e3811a42ceb
Lee hyeon kyu <leehyunguu@gmail.com>
2014-02-12 21:57:05 +0900

    1) modify Store_manager

--- git_test/Store_manager.cpp
+++ git_test/Store_manager.cpp
@@ -45,8 +45,28 @@ bool Store_manager::Store_Search(IN_Search &in_search, OUT_Search &out_search)
 
 bool Store_manager::Store_report(IN_Report &in_report, OUT_Report &out_report)
 {
-	//이미지 등록
+	//이미지 구조체를 선언
+	Imagelist imagelist;
+	//등록하기 전 검색해서 이미 있는 건물인지 찾아보기 위해 선언 
+	IN_Search in_search;
+	//이미지벡터를 지역변수로 선언
+	vector<Imagelist> Imagevector;
+
+	memcpy_s(in_search.cookie,64,in_report.cookie,64);
+	in_search.filter = in_report.filter;
+	memcpy_s(in_search.ID,21,in_report.ID,21);
+	in_search.store.image.buf = in_report.store.image.buf;
+	in_search.store.image.len = in_report.store.image.len;
+	in_search.store.latitude = in_report.store.latitude;
+	in_search.store.longitude = in_report.store.longitude;
+
+	//우선 등록된 이미지가 있는지 검색
+	dbm->Query_images(in_search, Imagevector);
+	//이미지벡터를 이용해 이미지 매니저를 불러 검색
+	//이미지 매니저(&이미지구조체, &매트릭스, 상점코드를 담은 벡터, &인서치);
+	//검색후 등록 안되있는 경우 이미지 등록
 	dbm->Query_image_register(in_report, out_report);
+	//등록 되어있는 경우 등록되어있다고 알림
 	return false;
 }
 

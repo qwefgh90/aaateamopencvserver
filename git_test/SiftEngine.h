f25e5425fc0099309779616dda436d944fdc8ad7
Lee hyeon kyu <leehyunguu@gmail.com>
2014-03-23 18:16:09 +0900

    1) good match size = 15

--- git_test/SiftEngine.h
+++ git_test/SiftEngine.h
@@ -6,7 +6,7 @@ class SiftEngine
 public:
 	static SiftEngine* getSiftEngine();
 
-	static const u_int MIN_MATCH = 20;
+	static const u_int MIN_MATCH = 15;
 
 	//return Matrix
 	bool createKey(__in Memory& memory,cv::Mat& m);	//Create a key for a Image
